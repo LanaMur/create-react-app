71a048cccb902c15f898f480c756c325f74966cd
LanaMur <lana.kalibri85@gmail.com>
2025-06-19 16:10:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Login from './Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import 'react-datepicker/dist/react-datepicker.css';
 import Header from "./Header";
 import Dashboard from "./Dashboard";
 
