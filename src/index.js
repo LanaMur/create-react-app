3e99f93dfd32d95099d0001a176ffd06fd3cc06b
LanaMur <lana.kalibri85@gmail.com>
2025-06-19 19:57:36 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CVGenerator from './App';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
